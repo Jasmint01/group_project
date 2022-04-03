f60f3d6371d03b0edf8ef8e1f4fb2c77fc4294c7
elainexe <33879747+elainexe@users.noreply.github.com>
2022-04-02 21:19:20 -0400

    menu item animation
    Now with polished animation on the collapsing/expanding

--- app.js
+++ app.js
@@ -23,15 +23,15 @@ window.addEventListener("resize", function(){
 for(let i=0;i<arrows.length;i++){
     arrows[i].addEventListener("click",function(){
         let arrow_block=arrows[i].parentElement.nextElementSibling;
-        if(arrow_block.style.display=="none"){
-            arrow_block.style.display="flex";
+        if(arrow_block.style.maxHeight=="0px"){ //using max height since we don't need a specific height. height="auto" does not allow for animation
             arrows[i].classList.remove("fa-caret-right");
             arrows[i].classList.add("fa-caret-down");
+            arrow_block.style.maxHeight="200px"; //default value is "none" but this does not allow for the animation
         }
         else{
-            arrow_block.style.display="none";
             arrows[i].classList.remove("fa-caret-down");
             arrows[i].classList.add("fa-caret-right");
+            arrow_block.style.maxHeight="0px";
         }
     });
 }
